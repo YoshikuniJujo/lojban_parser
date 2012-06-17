09445c3df7d2a6c94b64bf9181a3ef6561232bff
iocikun.jujon <PAF01143@nifty.ne.jp>
2012-06-18 06:33:40 +0900

    add some rules to fromOfficial.hs

--- fromOfficial.hs
+++ fromOfficial.hs
@@ -6,10 +6,8 @@ import Text.Peggy
 
 -------------------------------------------------------------------- 1396
 
-{-
-cmene :: String = !h &consonant_final coda? (any_syllable / digit)* &pause
-consonant_final :: () = (non_space &non_space)* consonant &pause
--}
+-- cmene :: String = !h &consonant_final coda? (any_syllable / digit)* &pause
+consonant_final :: () = (non_space &non_space)* consonant &pause	{ () }
 
 -------------------------------------------------------------------- 1408
 
@@ -26,7 +24,7 @@ consonant_final :: () = (non_space &non_space)* consonant &pause
 
 -- cmene :: String = !h &consonant_final coda? (any_syllable / digit)* &pause
 
--------------------------------------------------------------------- 1488
+-------------------------------------------------------------------- 1490
 
 {-
 any_syllable :: ()
@@ -39,19 +37,19 @@ consonantal_syllable :: ()
 coda :: (Maybe Char, Maybe Char)
 	= !any_syllable consonant &any_syllable	{ (Just $1, Nothing) }
 	/ syllabic? consonant? &pause		{ ($1, $2) }
+-}
 
 onset :: ()
-	= h
-	/ consonant? glide
-	/ initial
--}
+	= h				{ () }
+	/ consonant? glide		{ () }
+	/ initial			{ () }
 
 nucleus :: (Char, Maybe Char)
 	= vowel				{ ($1, Nothing) }
 	/ diphthong			{ (fst $1, Just $ snd $1) }
 	/ y !nucleus			{ ($1, Nothing) }
 
--------------------------------------------------------------------- 1521
+-------------------------------------------------------------------- 1523
 
 glide :: Char = (i / u) &nucleus !glide
 
@@ -66,17 +64,47 @@ o :: Char = comma* [oO]			{ $2 }
 u :: Char = comma* [uU]			{ $2 }
 y :: Char = comma* [yY]			{ $2 }
 
--------------------------------------------------------------------- 1541
+-------------------------------------------------------------------- 1543
 
--- consonant :: Char = voiced / unvoiced / syllabic
+cluster :: String = consonant consonant+	{ $1 : $2 }
 
+initial_pair :: (Char, Char) = &initial consonant consonant !consonant
+initial :: (Maybe Char, Maybe Char, Maybe Char)
+	= (affricate	{ (Just $ fst $1, Just $ snd $1, Nothing ) }
+		/ sibilant? other? liquid?) !consonant !glide
+
+affricate :: (Char, Char) = t c / t s / d j / d z
+
+liquid :: Char = l / r
+other :: Char = p / t !l / k / f / x / b / d !l / g / v / m / n !liquid
+sibilant :: Char = c / s !x / (j / z) !n !liquid
+
+consonant :: Char = voiced / unvoiced / syllabic
+
+syllabic :: Char = l / m / n / r
+voiced :: Char = b / d / g / j / v / z
 unvoiced :: Char = c / f / k / p / s / t / x
 
-l :: Char = comma* [lL] 
-b :: Char = comma* [bB] !h !b !unvoiced	{ $2 }
-h :: Char = comma* ['h] &nucleus	{ $2 }
+l :: Char = comma* [lL] !h !l			{ $2 }
+m :: Char = comma* [mM] !h !m !z		{ $2 }
+n :: Char = comma* [nN] !h !n !affricate	{ $2 }
+r :: Char = comma* [rR] !h !r			{ $2 }
+b :: Char = comma* [bB] !h !b !unvoiced		{ $2 }
+d :: Char = comma* [dD] !h !d !unvoiced		{ $2 }
+g :: Char = comma* [gG] !h !g !unvoiced		{ $2 }
+v :: Char = comma* [vV] !h !v !unvoiced		{ $2 }
+j :: Char = comma* [jJ] !h !j !z !unvoiced	{ $2 }
+z :: Char = comma* [zZ] !h !z !j !unvoiced	{ $2 }
+s :: Char = comma* [sS] !h !s !c !voiced	{ $2 }
+c :: Char = comma* [cC] !h !c !s !x !voiced	{ $2 }
+x :: Char = comma* [xX] !h !x !c !k !voiced	{ $2 }
+k :: Char = comma* [kK] !h !k !x !voiced	{ $2 }
+f :: Char = comma* [fF] !h !f !voiced		{ $2 }
+p :: Char = comma* [pP] !h !p !voiced		{ $2 }
+t :: Char = comma* [tT] !h !t !voiced		{ $2 }
+h :: Char = comma* ['h] &nucleus		{ $2 }
 
--------------------------------------------------------------------- 1601
+-------------------------------------------------------------------- 1603
 
 digit :: Char = comma* [0123456789] !h !nucleus
 					{ $2 }
@@ -101,18 +129,19 @@ space_char :: ()
 space_char1 :: () = '\t'		{ () }
 space_char2 :: () = '\r'		{ () }
 
--------------------------------------------------------------------- 1624
+-------------------------------------------------------------------- 1626
 
--- spaces :: ()
---	= !y initial_spaces
--- initial_spaces :: ()
---	= (comma* space_char / !ybu y)+ eof? / eof
--- ybu :: Lerfu = y space_char* bu		{ Lerfu }
+spaces :: () = !y initial_spaces				{ () }
+initial_spaces :: ()
+	= (comma* space_char { () } / !ybu y { () })+ eof?	{ () }
+	/ eof
+ybu :: Lerfu = y space_char* bu					{ Lerfu }
 -- lujvo :: String = !gismu !fuhivla brivla
 
--------------------------------------------------------------------- 1634
+-------------------------------------------------------------------- 1636
 
 -- bu :: () = &cmavo (b u) &post_word
+bu :: () = (b u)			{ () }
 
 |]
 
