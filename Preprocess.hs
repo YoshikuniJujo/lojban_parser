c780c0f07ed2f193e7d038de7814bf0f598828cc
iocikun.jujon <PAF01143@nifty.ne.jp>
2012-08-18 21:16:03 +0900

    make Preprocess

--- Preprocess.hs
+++ Preprocess.hs
@@ -13,8 +13,14 @@ preprocess src = do
 
 wordsToString :: [(String, String)] -> String
 wordsToString [] = ""
--- wordsToString (("zoi", _) :
+wordsToString (("zoi", s1) : (q, s2) : rest) =
+	"zoi" ++ s1 ++ "\NUL" ++ s2 ++ quoteInside q rest
 wordsToString ((w, s) : rest) = w ++ s ++ wordsToString rest
+
+quoteInside :: String -> [(String, String)] -> String
+quoteInside q0 ((q1, s) : rest)
+	| q0 == q1 = "\NUL" ++ s ++ wordsToString rest
+	| otherwise = q1 ++ s ++ quoteInside q0 rest
 
 lexwords :: String -> Maybe (String, [(String, String)])
 lexwords src = case lexerPappytest_words $ lexerPappyParse "test_words" src of
